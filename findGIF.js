224fe7f9421a453f30a7044b2e331ec16c20d4da
eliottolivieri <eliott.94@hotmail.com>
2018-07-04 15:47:46 -0400

    Animated GIF

--- findGIF.js
+++ findGIF.js
@@ -1,5 +1,5 @@
 
-var carList = ["Ferrari","Lamborghini","Pagani","Mclaren","Porsche","Aston Martin","Bentley","Rolce-Royce"];
+var carList = ["Ferrari","Lamborghini","Pagani","Mclaren","Porsche","GT86","Bentley"];
 
     for(var i = 0; i < carList.length; i++){
         var tempCar = carList[i];
@@ -22,20 +22,31 @@ $("button").on("click", function() {
 
         for (var i = 0; i < results.length; i++) {
           var gifDiv = $("<div class='item'>");
-
-         /* var rating = results[i].rating;
-
-          var p = $("<p>").text("Rating: " + rating);  */
-
-          var personImage = $("<img>");
+          var personImage = $("<img class='gifz'>");
           personImage.attr("src", results[i].images.fixed_height.url);
-
-         // gifDiv.prepend(p);
+          personImage.attr("data-still", results[i].images.fixed_height.url);
+          personImage.attr("data-animate", results[i].images.fixed_height.url);
+          personImage.attr("data-state", "still");
           gifDiv.prepend(personImage);
-
           $("#gifContainer").prepend(gifDiv);
         }
       });
+  });
+
+  // The function below does not work. I could not figure out how to make it work!
+  $(".gifz").on("click", function() {
+
+    console.log("You clicked a gif");
+    var state = $(this).attr("data-state");
+    console.log(state);
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    } else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
   });
 
 
